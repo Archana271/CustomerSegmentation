491e4ea0ac7b3b1ba3df2942a17901d82b92243e
Archana Bora <hima99glr@gmail.com>
2023-04-06 22:48:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,6 @@ if submitted:
         grid= sns.FacetGrid(cluster_df1, col='Cluster')
         grid= grid.map(plt.hist, c)
         plt.show()
-        return st.pyplot(fig)
+        st.pyplot(fig)
 
 
