7d9913a51432b11ccc60a46b7d508eaa31ee7fb5
Archana Bora <hima99glr@gmail.com>
2023-04-06 23:03:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,7 @@ if submitted:
     cluster_df1=df[df['Cluster']==clust]
     plt.figure(figsize=(20,3))
     for c in cluster_df1.drop(['Cluster'],axis=1):
-        
         fig, ax = plt.subplots()
-        ax.hist(arr, bins=20)
         grid= sns.FacetGrid(cluster_df1, col='Cluster')
         grid= grid.map(plt.hist, c)
         plt.show()
